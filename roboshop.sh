b93b00717cfc30921b82e743b77900199f57c343
sundhar <aitechwithsundhar@gmail.com>
2026-02-12 12:27:28 +0700

    actions

---
+++ roboshop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-096be3dc5a8288c12"
+
+for instance in "$@"
+do
+    echo "Launching instance: $instance"
+
+    instance_id=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Reservations[0].Instances[0].InstanceId' \
+        --output text)
+
+    echo "Instance ID: $instance_id"
+
+    if [[ "$instance" == "frontend" ]]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$instance_id" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
+        echo "Public IP of $instance: $IP"
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$instance_id" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
+        echo "Private IP of $instance: $IP"
+    fi
+done
